be6daa148d3fc55cfe0c6a18b9cb196d4f31f75f
Abdulkader Alabdo <134808102+Abdulkader5410@users.noreply.github.com>
2024-06-11 02:15:07 -0700

    Delete posts_app directory

--- posts_app/lib/core/strings/failure.dart
+++
@@ -1,3 +0,0 @@
-String SERVER_FAILURE_MESSAGE = "please try again later !";
-String EMPITY_CACHE_FAILURE_MESSAGE = "no data !";
-String OFFLINE_FAILURE_MESSAGE = "please check your internet connection !";
