b5b114fd99014e3b384331791bfa4ce9b9acf6cf
Abdulkader Alabdo <134808102+Abdulkader5410@users.noreply.github.com>
2024-06-11 02:14:29 -0700

    Delete chataty directory

--- chataty/android/app/src/main/kotlin/com/example/chataty/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.chataty
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- chataty/lib/features/contact/controller/contacts_controller.dart
+++
@@ -1,8 +0,0 @@
-import 'package:chataty/features/contact/repo/contact_repo.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final contactsControllerProvider = FutureProvider((ref) {
-  final contactsController = ref.watch(contactsRepoProvider);
-
-  return contactsController.getAllContacts();
-});
