fdbfb616826d7c950ee6793151fc600bb8b6ce6e
Abdulkader Alabdo <134808102+Abdulkader5410@users.noreply.github.com>
2024-06-11 02:14:49 -0700

    Delete ecommerce_app directory

--- ecommerce_app/android/app/src/main/kotlin/com/ecommerce_app/ecommerce_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.ecommerce_app.ecommerce_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- ecommerce_app/lib/core1/strings/messages.dart
+++
@@ -1,3 +0,0 @@
-String ADD_SUCCESSFUL_MESSAGE = "post added successfully";
-String DELETE_SUCCESSFUL_MESSAGE = "post deleted successfully";
-String UPDATE_SUCCESSFUL_MESSAGE = "post updated successfully";
